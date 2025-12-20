60ae372573e2627e1f90b06c8596cc64f81d19d4
KhiemNTK <khiem14205@gmail.com>
2025-12-20 23:40:19 +0700

    feat: integrate signup, signin form with auth store and navigation

--- frontend/src/components/auth/signin-form.tsx
+++ frontend/src/components/auth/signin-form.tsx
@@ -1,16 +1,17 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Label } from "../ui/label"
-
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "../ui/label";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useNavigate } from "react-router";
 
 const signInSchema = z.object({
-  username: z.string().min(3," Username is at least 3 characters long"),
-  password: z.string().min(6,"Password is at least 6 characters long"),
+  username: z.string().min(3, " Username is at least 3 characters long"),
+  password: z.string().min(6, "Password is at least 6 characters long"),
 });
 
 type SignInFormValues = z.infer<typeof signInSchema>;
@@ -19,50 +20,89 @@ export function SignInForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const { signIn } = useAuthStore();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormValues>({
+    resolver: zodResolver(signInSchema),
+  });
 
- const {register, handleSubmit, formState:{errors, isSubmitting}} = useForm<SignInFormValues>({
-  resolver: zodResolver(signInSchema),
-});
-
-const onSubmit = async (data: SignInFormValues) => {
-  // Handle form submit logic here
-}
+  const onSubmit = async (data: SignInFormValues) => {
+    // Handle form submit logic here
+    const { username, password } = data;
+    // Call signIn for signin
+    await signIn(username, password);
+    navigate("/");
+  };
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0 border-border">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8" onSubmit = {handleSubmit(onSubmit)}>
-          <div className="flex flex-col gap-6">
-            {/* Header */}
-            <div className="flex flex-col items-center text-center gap-2">
-              <a href="/" className="block mx-auto w-fit text-center">
-              <img src="/logo.svg" alt="logo" />
-              </a>
-              <h1 className="text-auto font-bold">Welcome back</h1>
-              <p className="text-muted-foreground text-balance">Welcome! Sign in to your Chime account</p>
-            </div>
-            {/*Username*/}
-            <div className="flex flex-col gap-3">
-                <Label htmlFor="username" className="block text-sm">Username</Label>
-                <Input type="text"id="username" placeholder="chime" {...register("username")}/>
-                {/*todo: add validation message*/}
-                {errors.username && (<p className="text-destructive text-sm">{errors.username.message}</p>)}
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              {/* Header */}
+              <div className="flex flex-col items-center text-center gap-2">
+                <a href="/" className="block mx-auto w-fit text-center">
+                  <img src="/logo.svg" alt="logo" />
+                </a>
+                <h1 className="text-auto font-bold">Welcome back</h1>
+                <p className="text-muted-foreground text-balance">
+                  Welcome! Sign in to your Chime account
+                </p>
               </div>
-            {/* Password */}
-             <div className="flex flex-col gap-3">
-                <Label htmlFor="password" className="block text-sm">Password</Label>
-                <Input type="password" id="password" {...register("password")}/>
+              {/*Username*/}
+              <div className="flex flex-col gap-3">
+                <Label htmlFor="username" className="block text-sm">
+                  Username
+                </Label>
+                <Input
+                  type="text"
+                  id="username"
+                  placeholder="chime"
+                  {...register("username")}
+                />
                 {/*todo: add validation message*/}
-                {errors.password && (<p className="text-destructive text-sm">{errors.password.message}</p>)}
+                {errors.username && (
+                  <p className="text-destructive text-sm">
+                    {errors.username.message}
+                  </p>
+                )}
               </div>
-            {/* Login Button */}
-            <Button type="submit" className="w-full" disabled= {isSubmitting}>Login</Button>
-            <div className="text-center text-sm">
-              Don't have an account? {""}
-              <a href="/signup" className="underline underline-offset-4 text-blue-900">Sign Up</a>
+              {/* Password */}
+              <div className="flex flex-col gap-3">
+                <Label htmlFor="password" className="block text-sm">
+                  Password
+                </Label>
+                <Input
+                  type="password"
+                  id="password"
+                  {...register("password")}
+                />
+                {/*todo: add validation message*/}
+                {errors.password && (
+                  <p className="text-destructive text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+              {/* Login Button */}
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
+                Login
+              </Button>
+              <div className="text-center text-sm">
+                Don't have an account? {""}
+                <a
+                  href="/signup"
+                  className="underline underline-offset-4 text-blue-900"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
-          </div>
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
@@ -78,5 +118,5 @@ const onSubmit = async (data: SignInFormValues) => {
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
-  )
+  );
 }
