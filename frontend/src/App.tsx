9b5e85064e1a0227b6c8407e5689a08c8d0140e4
KhiemNTK <khiem14205@gmail.com>
2025-12-21 09:19:20 +0700

    feat: add protected for sign in route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,29 @@
-import {BrowserRouter, Routes, Route} from 'react-router'
-import SignInPage from './pages/SignInPage'
-import SignUpPage from './pages/SignUpPage'
-import { ChatAppPage } from './pages/ChatAppPage';
-import { Toaster } from 'sonner';
+import { BrowserRouter, Routes, Route } from "react-router";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import { ChatAppPage } from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
-  return <>
-  <Toaster richColors/>
-  <BrowserRouter>
-  <Routes>
-    {/*Public routes */}
-    <Route path='/signin'
-    element= {<SignInPage/>}
-    />  
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/*Public routes */}
+          <Route path="/signin" element={<SignInPage />} />
 
-    <Route path='/signup'
-    element= {<SignUpPage/>}
-    />
+          <Route path="/signup" element={<SignUpPage />} />
 
-    {/*Protected routes}*/}
-    {/*todo: create protected route*/}
-    <Route path='/'
-    element= {<ChatAppPage/>}
-    />
-  </Routes>
-  </BrowserRouter>
-
-  </>;
+          {/*Protected routes}*/}
+          <Route element={<ProtectedRoute />} > 
+            <Route path="/" element={<ChatAppPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
