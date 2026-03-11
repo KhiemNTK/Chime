ad25acea2ccaa5810f91c6bb51df775145db075b
KhiemNTK <khiem14205@gmail.com>
2026-03-11 10:56:47 +0700

    feat: add sidebar template from shad cn

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,30 +1,5 @@
-import Logout from "@/components/auth/logout";
-import { Button } from "@/components/ui/button";
-import api from "@/lib/axios";
-import { useAuthStore } from "@/stores/useAuthStore";
-import { toast } from "sonner";
 
-export const ChatAppPage = () => {
-  const handleOnClick = async () => {
-    try {
-      await api.get("/users/test", { withCredentials: true });
-      toast.success("Test endpoint called successfully");
-    } catch (error) {
-      toast.error("Error calling test endpoint");
-      console.error(error);
-    }
-  };
-  const user = useAuthStore((s) => s.user);
-  return (
-    <div>
-      {user?.username}
-      <Logout />
 
-      <Button onClick={handleOnClick}>
-        Test
-      </Button>
-    </div>
-  );
-};
+export const ChatAppPage = () => {}
 
 export default ChatAppPage;
