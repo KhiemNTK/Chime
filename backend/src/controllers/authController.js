1cf5c63df11ac088a60602e088586e6e8e010b82
KhiemNTK <khiem14205@gmail.com>
2026-04-01 16:28:35 +0700

    fix: fix password's length requirement for signin and signup

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -15,6 +15,10 @@ export const signUp = async (req, res) => {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    }
+
     //check if user is exist
     const duplicateUser = await User.findOne({ username });
     if (duplicateUser) {
@@ -46,6 +50,10 @@ export const signIn = async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
       return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
     }
 
     //get hashed password to compare with password input
